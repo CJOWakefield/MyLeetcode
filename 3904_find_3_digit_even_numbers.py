66c789a9fea5275d77a66614f8c787534846b6da
Christian Wakefield <CJOWakefield@outlook.com>
2025-05-12 10:54:36 +0800

    Brute force implemented, marginally better given problem constraints.

--- 3904_find_3_digit_even_numbers.py
+++ 3904_find_3_digit_even_numbers.py
@@ -1,8 +1,10 @@
 from typing import List
 from collections import Counter
+import time
 
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
+
         def dp(pos, curr, counts):
             if pos == 3: return [curr] if curr % 2 == 0 else []
             results = []
@@ -17,9 +19,28 @@ class Solution:
                          for counts in [Counter(digits)]
                          for counts[d] in [counts[d] - 1]
                          for num in dp(1, d, counts)))
+
+    def findEvenNumbers_brute(self, digits: List[int]) -> List[int]:
+        counts = Counter(list(map(str, digits)))
+        res = []
+        for i in range(100, 1000, 2):
+            curr_counts = Counter(str(i))
+            if all(curr_counts[d] <= counts[d] for d in curr_counts):
+                res.append(i)
+        return res
     
 if __name__ == "__main__":
     sol = Solution()
+    start = time.time()
     print(sol.findEvenNumbers([2,1,3,0]))
     print(sol.findEvenNumbers([2,2,8,8,2]))
     print(sol.findEvenNumbers([3,7,5]))
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
+
+    start = time.time()
+    print(sol.findEvenNumbers_brute([2,1,3,0]))
+    print(sol.findEvenNumbers_brute([2,2,8,8,2]))
+    print(sol.findEvenNumbers_brute([3,7,5]))
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
