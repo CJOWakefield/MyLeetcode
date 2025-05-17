45a8835be7f9a00b792071e3ada1f00f337d0ec1
Christian Wakefield <CJOWakefield@outlook.com>
2025-05-17 08:39:40 +0800

    Daily medium.

--- 75_sort_colours.py
+++ 75_sort_colours.py
@@ -1,23 +1,20 @@
-def sortColors(nums: list[int]) -> None:
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    left, mid, right = 0, 0, len(nums)-1
-
-    while mid <= right:
-        if nums[mid] == 0:
-            nums[left], nums[mid] = nums[mid], nums[left]
-            left += 1
-            mid += 1
-        elif nums[mid] == 2:
-            nums[right], nums[mid] = nums[mid], nums[right]
-            right -= 1
-        else:
-            mid += 1
-
-    # return nums
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        left, mid, right = 0, 0, len(nums)-1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -= 1
+            else:
+                mid += 1
+        return nums
 
 # Notes: Simple solution, swapping based on three pointers thus O(n) time complexity. Initial attempt with for i in range(len(nums)) loop but mid pointer more effective.
 
 if __name__ == '__main__':
-    print(sortColors([1, 0, 0, 1, 2, 0, 1, 0, 2, 2, 0, 1]))
+    sol = Solution()
+    print(sol.sortColors([1, 0, 0, 1, 2, 0, 1, 0, 2, 2, 0, 1]))
