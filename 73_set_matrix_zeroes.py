34d444aa7360627583d3c585278c23b216661f89
Christian Wakefield <CJOWakefield@outlook.com>
2025-05-21 10:40:27 +0800

    Daily medium, grid. Improvement on last submission, with only row/col changes on actual zeroes.

--- 73_set_matrix_zeroes.py
+++ 73_set_matrix_zeroes.py
@@ -1,57 +1,48 @@
 import copy
+import time
 
-def setZeroes(matrix: list[list[int]]) -> None:
-    """
-    Do not return anything, modify matrix in-place instead.
-    """
-    base_matrix = copy.deepcopy(matrix)
-    m, n = len(matrix), len(matrix[0])
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
 
-    for i in range(m):
-        for j in range(n):
-            cell = base_matrix[i][j]
-            if cell == 0:
-                matrix[i] = [0 for _ in range(n)]
-                for q in range(m):
-                    matrix[q][j] = 0
+        # First attempt.
+        base_matrix = copy.deepcopy(matrix)
+        m, n = len(matrix), len(matrix[0])
 
-    print(matrix)
+        for i in range(m):
+            for j in range(n):
+                cell = base_matrix[i][j]
+                if cell == 0:
+                    matrix[i] = [0 for _ in range(n)]
+                    for q in range(m):
+                        matrix[q][j] = 0
+
+    def setZeroes_II(self, matrix: list[list[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+        to_change = set()
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0: to_change.add((i, j))
+        
+        for i, j in to_change:
+            matrix[i] = [0] * n
+            for row in matrix:
+                row[j] = 0
+
 
 # Notes: First attempt success. Poor timing complexity however due to iteration for each cell in matrix. Next attempt: Try to only iterate based on 0 values.
 
-# def optimal(matrix):
-
-#     m, n = len(matrix), len(matrix[0])
-
-#     row_zeroes = any(matrix[i][0] for i in range(n))
-#     col_zeroes = any(matrix[0][j] for j in range(m))
-
-#     for i in range(1, n):
-#         for j in range (1, m):
-#             if matrix[i][j] == 0:
-#                 matrix[i][0] = 0
-#                 matrix[0][j] = 0
-
-#     for i in range(1, n):
-#         if matrix[i][0] == 0:
-#             for j in range(1, m):
-#                 matrix[i][j] = 0
-
-#     for j in range(1, m):
-#         if matrix[0][j] == 0:
-#             for i in range(1, n):
-#                 matrix[i][j] = 0
-
-#     if row_zeroes:
-#         for j in range(m):
-#             matrix[0][j] = 0
-
-#     if col_zeroes:
-#         for i in range(n):
-#             matrix[i][0] = 0
-
-#     print(matrix)
-
 if __name__ == '__main__':
-    print(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
-    # print(optimal([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
+    sol = Solution()
+    start = time.time()
+    print(sol.setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
+    print(sol.setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
+    start = time.time()
+    print(sol.setZeroes_II([[1,1,1],[1,0,1],[1,1,1]]))
+    print(sol.setZeroes_II([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
